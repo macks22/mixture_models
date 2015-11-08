4ba1724a5c4cb6bb9a1f7a87917782b60926bb97
Mack Sweeney <mackenzie.sweeney@gmail.com>
2015-11-08 10:43:18 -0500

    add training loop and cli

--- rmix.py
+++ rmix.py
@@ -5,6 +5,9 @@ Implement mixture of regressions model from the paper:
     Scott Gaffney and Padhraic Smyth
 
 """
+import logging
+import argparse
+
 import numpy as np
 from scipy import stats
 
@@ -49,11 +52,48 @@ def gen_data(l=4, n=10):
     return X, y
 
 
+def make_parser():
+    parser = argparse.ArgumentParser(
+        description='train mixture of regressions on synthetic data')
+    parser.add_argument(
+        '-i', '--niters',
+        type=int, default=50,
+        help='max number of iterations to train for')
+    parser.add_argument(
+        '-e', '--epsilon',
+        type=float, default=None,
+        help='early stopping threshold for training; disabled by default')
+    parser.add_argument(
+        '-l', type=int, default=4,
+        help='number of trajectories per component')
+    parser.add_argument(
+        '-n', type=int, default=10,
+        help='number of time steps per trajectory')
+    parser.add_argument(
+        '-v', '--verbose',
+        type=int, default=1,
+        help='adjust verbosity of logging output')
+    return parser
+
+
 if __name__ == "__main__":
-    K = 3      # number of components
-    l = 4      # trajectories per component
+    parser = make_parser()
+    args = parser.parse_args()
+
+    # Setup logging.
+    logging.basicConfig(
+        level=(logging.DEBUG if args.verbose == 2 else
+               logging.INFO if args.verbose == 1 else
+               logging.ERROR),
+        format="[%(asctime)s]: %(message)s")
+
+    l = args.l
+    n = args.n
+
+    K = 3           # number of components
     M = l * K  # total number of trajectories
-    n = 10     # observations per trajectory
+    # l = 4      # trajectories per component
+    # n = 10     # observations per trajectory
     # N = M * n  # total number of observations
 
     # X and y are (entity x time x predictors).
@@ -67,65 +107,80 @@ if __name__ == "__main__":
 
     # Randomly init membership probs h_{jk}.
     # There are M * K of these.
-    # Note: this model already has per-row membership weights!
+    # Note: the posteriors of this model end up with per-row membership weights
     H = np.random.uniform(0, 1, (M, K))
     H = H / H.sum(axis=1)[:, np.newaxis].repeat(K, axis=1)
 
-    # Estimate B_k, \sigma_k^2, w_k from weighted least squares solutions,
-    # using current membership probs as weights.
-    # We construct H_k as an N x N matrix whose diagonal contains the
-    # weights to be applied to X and Y during regression.
+    prev_llik = -np.inf  # set to lowest possible number
+    llik = np.finfo(np.inf).min  # set to next lowest
 
-    # first calculate new weights.
-    H_sums = H.sum(axis=0)
-    weight = (1. / M) * H_sums
+    for iteration in range(args.niters):
 
-    for k in range(K):
-        H_k = np.diag(H[:, k].repeat(n, axis=0))
+        # Estimate B_k, \sigma_k^2, w_k from weighted least squares solutions,
+        # using current membership probs as weights.
+        # We construct H_k as an N x N matrix whose diagonal contains the
+        # weights to be applied to X and Y during regression.
 
-        # calculate B_k
-        tmp = X.T.dot(H_k)
-        p1 = np.linalg.inv(tmp.dot(X))
-        p2 = tmp.dot(y)
-        B[k] = p1.dot(p2)
+        # first calculate new weights.
+        H_sums = H.sum(axis=0)
+        weight = (1. / M) * H_sums
 
-        # calculate sigma_k^2
-        regress = X.dot(B[k])
-        residuals = y - regress
-        var[k] = residuals.T.dot(H_k).dot(residuals)
-
-    # divide sigmas by the membership weight sums for each component
-    var = var / H_sums
-
-    # Compute new membership probs using new param estimates.
-    H[:] = 1
-    X_rw = X.reshape((M, n, p))  # rw = row-wise
-    y_rw = y.reshape((M, n))
-    for j in range(M):
-        X_j = X_rw[j]
-        y_j = y_rw[j]
         for k in range(K):
-            means = X_j.dot(B[k])
-            sigma = np.sqrt(var[k])
-            H[k] *= weight[k]
-            for time in range(n):
-                H[j, k] *= stats.norm.pdf(y_j[time], means[time], sigma)
+            H_k = np.diag(H[:, k].repeat(n, axis=0))
 
-    # re-normalize the membership weights
-    H = H / H.sum(axis=1)[:, np.newaxis].repeat(K, axis=1)
+            # calculate B_k
+            tmp = X.T.dot(H_k)
+            p1 = np.linalg.inv(tmp.dot(X))
+            p2 = tmp.dot(y)
+            B[k] = p1.dot(p2)
 
-    # Loop to step 2 until log-likelihood stabilizes.
-    # Calculate log-likelihood.
-    lliks = np.ones(M)
-    for j in range(M):
-        X_j = X_rw[j]
-        y_j = y_rw[j]
-        for k in range(K):
-            means = X_j.dot(B[k])
-            sigma = np.sqrt(var[k])
-            lliks[j] *= weight[k]
-            for time in range(n):
-                lliks[j] *= stats.norm.pdf(y_j[time], means[time], sigma)
+            # calculate sigma_k^2
+            regress = X.dot(B[k])
+            residuals = y - regress
+            var[k] = residuals.T.dot(H_k).dot(residuals)
 
-    llik = np.log(lliks).sum()
+        # divide sigmas by the membership weight sums for each component
+        var = var / H_sums
+
+        # Compute new membership probs using new param estimates.
+        H[:] = 1
+        X_rw = X.reshape((M, n, p))  # rw = row-wise
+        y_rw = y.reshape((M, n))
+        for j in range(M):
+            X_j = X_rw[j]
+            y_j = y_rw[j]
+            for k in range(K):
+                means = X_j.dot(B[k])
+                sigma = np.sqrt(var[k])
+                H[k] *= weight[k]
+                for time in range(n):
+                    H[j, k] *= stats.norm.pdf(y_j[time], means[time], sigma)
+
+        # re-normalize the membership weights
+        H = H / H.sum(axis=1)[:, np.newaxis].repeat(K, axis=1)
+
+        # Loop to step 2 until log-likelihood stabilizes.
+        # Calculate log-likelihood.
+        lliks = np.ones(M)
+        for j in range(M):
+            X_j = X_rw[j]
+            y_j = y_rw[j]
+            for k in range(K):
+                means = X_j.dot(B[k])
+                sigma = np.sqrt(var[k])
+                lliks[j] *= weight[k]
+                for time in range(n):
+                    lliks[j] *= stats.norm.pdf(y_j[time], means[time], sigma)
+
+        llik = np.log(lliks).sum()
+        logging.info('%d: log-likelihood: %.4f' % (iteration, llik))
+        if np.isinf(llik):
+            raise ValueError('log-likelihood has become infinite')
+
+        # early stopping check if enabled
+        if args.epsilon is not None:
+            if llik - prev_llik <= args.epsilon:
+                break
+            else:
+                prev_llik = llik
 
