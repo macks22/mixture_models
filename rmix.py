6b2b3274819b201c48edd3d7fc9cba5d481b96d5
Mack Sweeney <mackenzie.sweeney@gmail.com>
2015-11-25 15:37:16 -0500

    better convergence checks

--- rmix.py
+++ rmix.py
@@ -39,13 +39,14 @@ def gen_data(l=4, n=10):
             for time in range(n):
                 samples[idx, time] = model(xs[idx, time])
 
-    X = np.zeros((N, 3))
+    X = np.zeros((N, 4))
     y = np.zeros(N)
     X[:, 0] = 1  # intercept term
     idx = 0
     for (traj, time), y_sample in np.ndenumerate(samples):
         X[idx, 1] = time
         X[idx, 2] = xs[traj, time]
+        X[idx, 3] = xs[traj, time] ** 2
         y[idx] = y_sample
         idx += 1
 
@@ -57,7 +58,7 @@ def make_parser():
         description='train mixture of regressions on synthetic data')
     parser.add_argument(
         '-i', '--niters',
-        type=int, default=50,
+        type=int, default=None,
         help='max number of iterations to train for')
     parser.add_argument(
         '-e', '--epsilon',
@@ -99,11 +100,28 @@ if __name__ == "__main__":
     # X and y are (entity x time x predictors).
     X, y = gen_data(l, n)
     N, p = X.shape
+    X_rw = X.reshape((M, n, p))  # rw = row-wise
+    y_rw = y.reshape((M, n))
 
     # Init params to 0s.
     B = np.ndarray((K, p))
     var = np.ndarray(K)
     weight = np.ndarray(K)
+
+    # params = {
+    #     'B': [],
+    #     'var': [],
+    #     'w': []
+    # }
+
+    # handle divide by 0 errors
+    np.seterr(divide='raise')  # raise divide by 0 errors
+    eps = np.finfo(float).eps  # add to denominators which may go to 0
+
+    # We allow minor fluctuation of log-likelihood for stopping.
+    # It can increase for one iteration; we'll stop on second.
+    # We track it with a counter.
+    bad_iterations = 0
 
     # Randomly init membership probs h_{jk}.
     # There are M * K of these.
@@ -114,7 +132,9 @@ if __name__ == "__main__":
     prev_llik = -np.inf  # set to lowest possible number
     llik = np.finfo(np.inf).min  # set to next lowest
 
-    for iteration in range(args.niters):
+    niters = np.iinfo(np.int).max if args.niters is None else args.niters
+    iter_range = xrange(niters)
+    for iteration in iter_range:
 
         # Estimate B_k, \sigma_k^2, w_k from weighted least squares solutions,
         # using current membership probs as weights.
@@ -142,17 +162,18 @@ if __name__ == "__main__":
         # divide sigmas by the membership weight sums for each component
         var = var / H_sums
 
+        # params['B'].append(B)
+        # params['var'].append(var)
+        # params['w'].append(weight)
+
         # Compute new membership probs using new param estimates.
-        H[:] = 1
-        X_rw = X.reshape((M, n, p))  # rw = row-wise
-        y_rw = y.reshape((M, n))
+        H[:] = weight
         for j in range(M):
             X_j = X_rw[j]
             y_j = y_rw[j]
             for k in range(K):
                 means = X_j.dot(B[k])
                 sigma = np.sqrt(var[k])
-                H[k] *= weight[k]
                 for time in range(n):
                     H[j, k] *= stats.norm.pdf(y_j[time], means[time], sigma)
 
@@ -160,27 +181,39 @@ if __name__ == "__main__":
         H = H / H.sum(axis=1)[:, np.newaxis].repeat(K, axis=1)
 
         # Loop to step 2 until log-likelihood stabilizes.
-        # Calculate log-likelihood.
-        lliks = np.ones(M)
+        # Calculate expectation of complete data log-likelihood.
+        llik = 0
+        lliks = np.zeros((M, K))
         for j in range(M):
             X_j = X_rw[j]
             y_j = y_rw[j]
             for k in range(K):
+                llik += H[j, k] * np.log(weight[k])
+
                 means = X_j.dot(B[k])
                 sigma = np.sqrt(var[k])
-                lliks[j] *= weight[k]
                 for time in range(n):
-                    lliks[j] *= stats.norm.pdf(y_j[time], means[time], sigma)
+                    lliks[j, k] += np.log(
+                        stats.norm.pdf(y_j[time], means[time], sigma) + eps)
 
-        llik = np.log(lliks).sum()
+        lliks *= H
+        llik += lliks.sum()
         logging.info('%d: log-likelihood: %.4f' % (iteration, llik))
         if np.isinf(llik):
             raise ValueError('log-likelihood has become infinite')
 
-        # early stopping check if enabled
-        if args.epsilon is not None:
-            if llik - prev_llik <= args.epsilon:
+        # early stopping check if enabled (after iteration #5)
+        if args.epsilon is not None and iteration > 5:
+            diff = llik - prev_llik
+            if diff == 0:  # done
                 break
+            elif diff <= args.epsilon:
+                # allow small reductions which could be due to rounding error
+                if diff < -0.01:
+                    bad_iterations += 1
+                    if bad_iterations > 1:
+                        break
             else:
+                bad_iterations = 0
                 prev_llik = llik
 
